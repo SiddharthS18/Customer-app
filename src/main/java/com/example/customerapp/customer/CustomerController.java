9c55d556544492327710d55b46e5510ec50305e7
SiddharthS18 <siddharth.sharma1805@gmail.com>
2023-01-31 23:04:21 +0530

    CRUD Operations added

--- src/main/java/com/example/customerapp/customer/CustomerController.java
+++ src/main/java/com/example/customerapp/customer/CustomerController.java
@@ -1,7 +1,6 @@
 package com.example.customerapp.customer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,5 +17,22 @@ public class CustomerController {
     @GetMapping("/")
     List<Customer> getCustomer(){
         return customerService.getCustomer();
+    }
+
+    @PostMapping
+    void createNewCustomer(@RequestBody Customer customer){
+        System.out.println("POST REQUEST...");
+        System.out.println(customer);
+    }
+
+    @PutMapping
+    void updateNewCustomer(@RequestBody Customer customer){
+        System.out.println("PUT REQUEST...");
+        System.out.println(customer);
+    }
+
+    @DeleteMapping(path="{customerId}")
+    void DeleteMapping(@PathVariable("customerId") Long id){
+        System.out.println("DELETE REQUEST FOR CUSTOMER ID " + id);
     }
 }
